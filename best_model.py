b8b804738a8ef322eff86e78c7f3236b81c300ce
Poppeloppen <victor.popp.henriksen@gmail.com>
2022-05-02 17:33:42 +0200

    Trying to make MLproject work on Azure

--- best_model.py
+++ best_model.py
@@ -1,4 +1,9 @@
 """Look through all the mlflow runs on azure and save the best model"""
 
+#for azure machine learning studio experiment tracking
+from azureml.core import Workspace
+ws = Workspace.from_config()
+mlflow.set_tracking_uri(ws.get_mlflow_tracking_uri())
+
 print("EEEEEYYYYYY, U managed to call the 'best_model.py' script. WAY TO GO!!!")
 
--- get_pred.py
+++ get_pred.py
@@ -1,4 +1,8 @@
 """Should contain the pipeline and the best performing model, should be able to get prediction"""
 
+#for azure machine learning studio experiment tracking
+from azureml.core import Workspace
+ws = Workspace.from_config()
+mlflow.set_tracking_uri(ws.get_mlflow_tracking_uri())
 
 print("THIS IS JUST FOR TESTING, you are in 'get_pred.py')
