efc578db2fc455690ea79706a34e0e7f861f7f24
Poppeloppen <victor.popp.henriksen@gmail.com>
2022-05-02 17:25:45 +0200

    Additional entry points for MLproject and corresponding files

---
+++ best_model.py
@@ -0,0 +1,4 @@
+"""Look through all the mlflow runs on azure and save the best model"""
+
+print("EEEEEYYYYYY, U managed to call the 'best_model.py' script. WAY TO GO!!!")
+
---
+++ get_pred.py
@@ -0,0 +1,4 @@
+"""Should contain the pipeline and the best performing model, should be able to get prediction"""
+
+
+print("THIS IS JUST FOR TESTING, you are in 'get_pred.py')
