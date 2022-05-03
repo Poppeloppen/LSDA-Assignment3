2e0935f971b80efc22ee8dd39e93378811184d93
Poppeloppen <victor.popp.henriksen@gmail.com>
2022-05-03 13:02:21 +0200

    Query azure to get the metrics of all runs

--- best_model.py
+++ best_model.py
@@ -2,9 +2,23 @@
 import mlflow
 
 #for azure machine learning studio experiment tracking
-from azureml.core import Workspace
+from azureml.core import Workspace, Experiment
 ws = Workspace.from_config()
 mlflow.set_tracking_uri(ws.get_mlflow_tracking_uri())
 
 print("EEEEEYYYYYY, U managed to call the 'best_model.py' script. WAY TO GO!!!")
+
+print(ws.experiments)
+
+
+
+
+for exp in ws.experiments:
+    experiment = Experiment(ws, exp)
+   
+
+    for run in experiment.get_runs():
+        print(run.get_metrics()["mean_MSE"])
+
+
 
